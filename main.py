3ffc860385a52dd77afb52ccb5852d30a5ea9026
EmilyJaneBeasley <mizem1977@yahoo.com>
2017-09-19 17:42:19 -0500

    Add hello form

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-from flask import Flask,request
+from flask import Flask, request
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
+app.config['DEBUG'] = True 
 
 form = """
 <!doctype html>
-    <html>
-        <body>
-            <form action="/hello" method="post">
+<html>
+    <body>
+        <form action="/hello" method="post">
             <label for="first-name">First Name:</label>
-                <input id="first-name" type="text" name="first_name"/>
-                <input type="submit">
-            </form>
-        </body>
-    </html>
+        <input id="first-name" type="text" name="first_name" />
+        <input type="submit"/>
+        </form>
+    </body>
+</html>
 """
 
 @app.route("/")
@@ -23,6 +23,37 @@ def index():
 @app.route("/hello", methods=['POST'])
 def hello():
     first_name = request.form['first_name']
-    return "<h1>Hello, " + first_name + "</h1>"
+    return '<h1>Hello, ' + first_name + '</h1>'
+    
 
 app.run()
+    
+    
+
+    #time_form = """ <!DOCTYPE html>
+       # <html>
+           # <style>
+            #    .error {{color:red}}
+           # </style>
+        # <h1>Validate Time</h1>
+   # <form method='POST'>
+      #  <label>Hours (24 hour format)
+         #   <input name="hours" type="text" value='{hours}' />
+      #  </label>
+       # <p class="error">{hours_error}</p>
+       # <label>Minutes
+        #    <input name="minutes" type="text" value='{minutes}' />
+       # </label>
+       # <p class="error">{minutes_error}</p>
+       # input type="submit" value="Validate" />
+  #  </form>
+  #  </html>
+  #  """
+    
+
+#@app.route("/validate-time")
+
+#def display_time_form():
+ #       return time_form.format(hours='', hours_error='', 
+  #      minutes='', minutes_error='')
+#app.run()
